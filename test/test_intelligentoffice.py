ef75069afde840eb422e86aae5658e7340e4694a
richard.kudela <richard.kudela@yunextraffic.com>
2024-11-27 15:51:24 +0100

    US1 Red phase

--- test/test_intelligentoffice.py
+++ test/test_intelligentoffice.py
@@ -1,15 +1,32 @@
 import unittest
-from datetime import datetime
-from unittest.mock import patch, Mock, PropertyMock
+from unittest.mock import patch, Mock
+
 import mock.GPIO as GPIO
-from mock.SDL_DS3231 import SDL_DS3231
-from mock.adafruit_veml7700 import VEML7700
 from src.intelligentoffice import IntelligentOffice, IntelligentOfficeError
 
 
 class TestIntelligentOffice(unittest.TestCase):
 
+    def test_check_quadrant_occupancy_wrong_pin_low(self):
+        io = IntelligentOffice()
+        self.assertRaises(IntelligentOfficeError, io.check_quadrant_occupancy, 10)
+
+    def test_check_quadrant_occupancy_wrong_pin_between(self):
+        io = IntelligentOffice()
+        self.assertRaises(IntelligentOfficeError, io.check_quadrant_occupancy, 14)
+
+    def test_check_quadrant_occupancy_wrong_pin_high(self):
+        io = IntelligentOffice()
+        self.assertRaises(IntelligentOfficeError, io.check_quadrant_occupancy, 16)
+
     @patch.object(GPIO, "input")
-    def test_something(self, mock_object: Mock):
-        # This is an example of test where I want to mock the GPIO.input() function
-        pass
+    def test_check_quadrant_occupancy_true(self, mock_object: Mock):
+        mock_object.return_value = True
+        io = IntelligentOffice()
+        self.assertTrue(io.check_quadrant_occupancy(io.INFRARED_PIN1))
+
+    @patch.object(GPIO, "input")
+    def test_check_quadrant_occupancy_false(self, mock_object: Mock):
+        mock_object.return_value = False
+        io = IntelligentOffice()
+        self.assertFalse(io.check_quadrant_occupancy(io.INFRARED_PIN3))
