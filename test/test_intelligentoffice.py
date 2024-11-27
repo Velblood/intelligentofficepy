8434a8dcb83a242a7018ddaabf27133eeca0e28c
richard.kudela <richard.kudela@yunextraffic.com>
2024-11-27 16:07:55 +0100

    US2 Red phase

--- test/test_intelligentoffice.py
+++ test/test_intelligentoffice.py
@@ -1,7 +1,9 @@
 import unittest
+from datetime import datetime
 from unittest.mock import patch, Mock
 
 import mock.GPIO as GPIO
+from mock.SDL_DS3231 import SDL_DS3231
 from src.intelligentoffice import IntelligentOffice, IntelligentOfficeError
 
 
@@ -30,3 +32,35 @@ class TestIntelligentOffice(unittest.TestCase):
         mock_object.return_value = False
         io = IntelligentOffice()
         self.assertFalse(io.check_quadrant_occupancy(io.INFRARED_PIN3))
+
+    @patch.object(SDL_DS3231, "read_datetime")
+    def test_check_blinds_closed(self, mock_datetime: Mock):
+        mock_datetime.return_value = datetime(2024, 11, 27, 7, 59, 59, 59)
+        io = IntelligentOffice()
+        io.manage_blinds_based_on_time()
+        self.assertFalse(io.blinds_open)
+
+    @patch.object(IntelligentOffice, "change_servo_angle")
+    @patch.object(SDL_DS3231, "read_datetime")
+    def test_check_blinds_opened_in_normal_day(self, mock_datetime: Mock, mock_servo: Mock):
+        mock_datetime.return_value = datetime(2024, 11, 27, 8, 0, 0)
+        io = IntelligentOffice()
+        io.manage_blinds_based_on_time()
+        mock_servo.assert_called_with(12)
+        self.assertTrue(io.blinds_open)
+
+    @patch.object(IntelligentOffice, "change_servo_angle")
+    @patch.object(SDL_DS3231, "read_datetime")
+    def test_check_blinds_closed_evening_in_normal_day(self, mock_datetime: Mock, mock_servo: Mock):
+        mock_datetime.return_value = datetime(2024, 11, 27, 20, 0, 0)
+        io = IntelligentOffice()
+        io.manage_blinds_based_on_time()
+        mock_servo.assert_called_with(2)
+        self.assertFalse(io.blinds_open)
+
+    @patch.object(SDL_DS3231, "read_datetime")
+    def test_check_blinds_closed_weekend(self, mock_datetime: Mock):
+        mock_datetime.return_value = datetime(2024, 11, 30, 20, 0, 0)
+        io = IntelligentOffice()
+        io.manage_blinds_based_on_time()
+        self.assertFalse(io.blinds_open)
